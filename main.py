841c1e9e6df82050d7d15f3590f38cbba4651c2b
clara <clarabertholin@gmail.com>
2024-03-26 10:29:06 -0300

    26/03 2.0

--- main.py
+++ main.py
@@ -13,4 +13,11 @@ cliente.on_connect = ao_conectar
 cliente.on_message = ao_receber
 cliente.connect("broker.hivemq.com", 1883, 60)
 cliente.subscribe("aula3a")
-cliente.loop_forever()
+#cliente.loop_forever()
+
+cliente.loop_start()
+while True:
+    cliente.publish("aula3a", input("escreva uma mensagem: "))
+
+cliente.loop_finish()
+
