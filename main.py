190d3a4f59533215f5bf19428627cb69e0a329e5
clara <clarabertholin@gmail.com>
2024-03-26 10:04:55 -0300

    26/03

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import paho.mqtt.client as mqtt
 
-def ao_conectar(cliente, userdata, flags, rc):
-    print("Nos conectamos com o seguinte codigo de resultado {}".format(str(rc)))
+def ao_conectar(client, userdata, flags, rc):
+    print("Nos conectamos com o seguinte código de resultado {}".format(str(rc)))
 
 
-def ao_receber(cliente, userdata, msg):
-    print("{} --- {}".format(msg.topic, str(msg.payload)))
+def ao_receber(client, userdata, msg):
+     print("{} --- {}".format(msg.topic, str(msg.payload)))
+
+cliente = mqtt.Client()
+
+cliente.on_connect = ao_conectar
+cliente.on_message = ao_receber
+cliente.connect("broker.hivemq.com", 1883, 60)
+cliente.subscribe("aula3a")
+cliente.loop_forever()
